9005ddea35423d7cb681b95cc4f464f0d9106148
Tyler Westland <tylerofthewest@gmail.com>
2021-10-09 12:19:59 -0400

    Maybe plays music files?

--- src/maker/music/music.py
+++ src/maker/music/music.py
@@ -7,6 +7,8 @@ import argparse
 import os
 import sys
 
+from pygame import mixer
+
 
 def parse_arguments(args=None) -> None:
     """Returns the parsed arguments.
@@ -51,8 +53,35 @@ def main(input_file:str, quiet:bool=False, output_file:str="output") -> None:
     # Error check if the file even exists
     if not os.path.isfile(input_file):
         raise FileNotFoundError("File not found: {}".format(input_file))
-    with open(input_file) as fin:
-        print(fin.read())
+
+    # Starting the mixer
+    mixer.init()
+      
+    # Loading the song
+    mixer.music.load(input_file)
+      
+    # Setting the volume
+    mixer.music.set_volume(0.7)
+      
+    # Start playing the song
+    mixer.music.play()
+      
+    # infinite loop
+    while True:
+        print("Press 'p' to pause, 'r' to resume")
+        print("Press 'e' to exit the program")
+        query = input("  ")
+          
+        if query == 'p':
+            # Pausing the music
+            mixer.music.pause()     
+        elif query == 'r':
+            # Resuming the music
+            mixer.music.unpause()
+        elif query == 'e':
+            # Stop the mixer
+            mixer.music.stop()
+            break
 
     return None
 
